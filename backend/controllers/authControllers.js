8570b7a72693fbe1c8addefb9ae71e91cd313fc7
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-29 22:34:50 +0530

    added login routing

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -14,14 +14,16 @@ export const signUp = async (req, res) => {
     if (user) {
       return res.status(400).json({ error: "Username already exsist" });
     }
-
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName: fullName,
       username: username,
-      password: password,
+      password: hashedPassword,
       gender: gender,
       profilePic: gender === "male" ? girlProfilePic : boyProfilePic,
     });
@@ -34,7 +36,7 @@ export const signUp = async (req, res) => {
     //   profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     // });
     if (newUser) {
-       generateTokenAndSetCookie(newUser._id, res);
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
@@ -50,11 +52,37 @@ export const signUp = async (req, res) => {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
-//   res.send("loding user");
+  //   res.send("loding user");
 };
 
 export const loginUser = async (req, res) => {
-  res.send("ye to signUp hai");
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+
+
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+  //   res.send("ye to signUp hai");
 };
 export const logOut = async (req, res) => {
   res.send("ye to logOut hai");
