a035ce3962c3dff76bf0789f068cad360939578d
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-03-30 21:34:07 +0530

    complited message controller and fix the bug ogf sender id

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,31 +1,38 @@
 import Conversation from "../models/converModel.js";
 import Message from "../models/messageModel.js";
-
-export const sendMessage = async (req, res) => {
+// import protectRoute from "../middleware/protectRoute.js";
+const sendMessage = async (req, res) => {
   // res.send("message send")
   // console.log("message send")
 
+  // console.log(req.user._id);
   try {
     const {message}=req.body;
     const {id:receiverId}=req.params;
-    const {senderId}=req.user._id;
+    const senderId=req.user._id;
+
 
     let conversation = await Conversation.findOne({
-        participants:[senderId,receiverId]
+      participants:{$all:[senderId,receiverId]}
     });
-
+    
     if(!conversation){
-        conversation = await Conversation.create({
-            participants:{$all:[senderId,receiverId]}
-        })
+      conversation = await Conversation.create({
+          participants:[senderId,receiverId]
+        });
     };
     const newMessage= new Message({
-        senderId,receiverId,message
+        senderId:senderId,
+        receiverId:receiverId,
+        message:message,
     });
 
     if(newMessage){
         conversation.messages.push(newMessage._id)
     }
+   await conversation.save();
+   await newMessage.save();
+
    res.status(201).json(newMessage)
    console.log("megga")
   } 
@@ -35,3 +42,5 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export default sendMessage
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,28 +1,36 @@
-import Jwt  from "jsonwebtoken";
-import User from "../models/userModel.js"
-export const protectRoute=async(req,res,next)=>{
- try {
-    const token= req.cookies.jwt;
+import Jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
-    if(!token){
-        return res.status(401).json({error:"Unauthorize - no token provided"})
+
+ const protectRoute = async (req, res, next) => {
+  try {
+
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorize - no token provided" });
     }
 
-    const decoded=Jwt.verify(token,process.env.JWT_SECRET)
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET);
 
-    if (!decoded){
-        return res.status(401).json({error:"Unauthorize - invalid token "})
+    if (!decoded) {
+      return res.status(401).json({ error: "Unauthorize - invalid token " });
     }
 
-    const user=await User.findById(decoded.userId).select("-password")
+    const user = await User.findById(decoded.userId).select("-password");
 
-    if(!user){
-        return res.status(401).json({error:"User not found"})
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
     }
-    req.user=user;
+    req.user = user;
+    // console.log(req.user,"from protect")
     next();
- } catch (error) {
+
+
+  } catch (error) {
     console.log("Error in protectRoute controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
- }
-}
+  }
+};
+
+export default protectRoute;
